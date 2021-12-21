0194ab72792a5444dd6d152f4e3e750d96841e5a
Alex-anglik <95584937+Alex-anglik@users.noreply.github.com>
2021-12-21 04:39:01 +0100

    update in script

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ function compute()
     
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
-    var year = document.getElementById("years").value;
+    var years = document.getElementById("years").value;
     var interest = principal *years * rate /100;
     var year = new Date().getFullYear() + parseInt(years);
+    const amount = parseInt(principal) + interest;
 
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\
-    <br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\
-    <br\>in the year "+year+"\<br\>"
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+
 }
 
 function updateRate(){
